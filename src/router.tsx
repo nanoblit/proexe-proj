091cc87d4e98521e8dc1aa882c564dfd4c0aeb4d
nanoblit <jakub.k.maleta@gmail.com>
2023-05-17 22:00:56 +0200

    Fix for GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,28 +1,25 @@
-import { createBrowserRouter, createHashRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, RouteObject } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Layout from "./pages/Layout.tsx";
 
-const routes: RouteObject[] = [
-  {
-    path: "",
-    element: <Layout/>,
-    errorElement: <>404</>,
-    children: [
-      {
-        path: "",
-        element: <Home/>,
-      },
-      {
-        path: "form",
-        element: <>form</>,
-      },
-    ],
-  },
-];
+const routes: RouteObject[] =
+  [
+    {
+      path: "",
+      element: <Layout/>,
+      errorElement: <>404</>,
+      children: [
+        {
+          path: "",
+          element: <Home/>,
+        },
+        {
+          path: "form",
+          element: <>form</>,
+        },
+      ],
+    },
+  ];
 
-const options = { basename: import.meta.env.BASE_URL };
-
-const router = import.meta.env.DEV ? createBrowserRouter(routes, options) : createHashRouter(routes, options);
-
-console.log(import.meta.env.BASE_URL);
+const router = createBrowserRouter(routes, { basename: import.meta.env.BASE_URL });
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/proj11321321/",
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/proj11321321/";
+  }
+
+  return config;
 });
